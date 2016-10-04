ce817ba904edf3a603c2a76c34112126a6963955
#manosenelcódigo <peligrosamente@gmail.com>
2016-10-03 22:09:06 -0300

    Create Calculos.cs

---
+++ App_Code/Calculos.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class Calculos
+{
+    //atributos o propiedades de una clase
+    //por convención los atributos deberían ser de tipo private siempre
+    public int numero1;
+    private int numero2;
+    private string nombreCompleto;
+    
+    //ésto es un constructor de la clase
+    public Calculos()
+    {
+        this.numero1 = 13;
+        this.numero2 = 12;
+    }
+
+    //métodos de ejecución, de retorno o estáticos
+}
